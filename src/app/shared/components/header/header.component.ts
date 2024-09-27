ca35480cb54b0f58e63a31d8d738e7c8b86d0741
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-09-27 09:49:28 +0200

    first section

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
+  standalone: true, 
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  currentLanguage = 'EN'; // Standardmäßig Englisch
 
+  switchLanguage(lang: string) {
+    this.currentLanguage = lang;
+    console.log(`Language switched to: ${lang}`);
+    // Füge hier deine Sprachumschaltung-Logik hinzu (z.B. Nutzung eines Translation-Services)
+  }
 }
