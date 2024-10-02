d9ad7ba381f0b5c005c55acf24173790a1ac9dff
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-10-02 09:20:52 +0200

    projects

--- src/app/mainpage/projects/projects.component.ts
+++ src/app/mainpage/projects/projects.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  // Hover-Status für das "Join"-Element
+  isHoveredJoin: boolean = false;
+  // Hover-Status für das "El Pollo Loco"-Element
+  isHoveredPollo: boolean = false;
 
+  // Methoden für "Join"
+  onMouseEnterJoin() {
+    this.isHoveredJoin = true;
+  }
+
+  onMouseLeaveJoin() {
+    this.isHoveredJoin = false;
+  }
+
+  // Methoden für "El Pollo Loco"
+  onMouseEnterPollo() {
+    this.isHoveredPollo = true;
+  }
+
+  onMouseLeavePollo() {
+    this.isHoveredPollo = false;
+  }
 }
