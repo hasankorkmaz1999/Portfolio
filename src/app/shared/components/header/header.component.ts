27e86f7c7302a31d2e16c563e3b4c5b5ab6d78df
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-10-10 09:18:29 +0200

    header responsive

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,19 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true, 
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  currentLanguage = 'EN'; // Standardmäßig Englisch
+  isMenuOpen = false; // Menü ist standardmäßig geschlossen
+  currentLanguage = 'EN'; // Standardmäßig ist die Sprache auf EN gesetzt
 
-  switchLanguage(lang: string) {
-    this.currentLanguage = lang;
-    console.log(`Language switched to: ${lang}`);
-    // Füge hier deine Sprachumschaltung-Logik hinzu (z.B. Nutzung eines Translation-Services)
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Menü umschalten
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false; // Menü schließen, wenn auf Overlay geklickt wird
+  }
+
+  switchLanguage(language: string) {
+    this.currentLanguage = language; // Sprache umschalten
+    console.log(`Sprache umgeschaltet auf: ${language}`);
   }
 }
