365ec301997d46c09ee73b51d3443cfb6f7c2367
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-10-14 16:04:25 +0200

    .

--- src/app/mainpage/contact/contact.component.ts
+++ src/app/mainpage/contact/contact.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { inject } from '@angular/core';
 import { TranslationService } from '../../translation.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -35,8 +36,51 @@ export class ContactComponent {
   }
 
   // Funktion, um die Eingabe zu stoppen
-  stopInput(): void {
-    this.isInputStopped = true;
-    this.showCheckmark = false; // Verstecke das Häkchen, wenn die Eingabe gestoppt wird
+ 
+
+  formData = {
+    name: '',
+    email: '',
+    message: '',
+    privacyAccepted: false,
+  };
+
+  // Zustände, um zu prüfen, ob die Felder validiert wurden
+  nameChecked = false;
+  emailChecked = false;
+  messageChecked = false;
+
+  validateInput(control: any): void {
+    if (control.name === 'name') {
+      this.nameChecked = true;
+    }
+    if (control.name === 'email') {
+      this.emailChecked = true;
+    }
+    if (control.name === 'message') {
+      this.messageChecked = true;
+    }
   }
+
+  confirmationVisible = false; // Zustand für die Bestätigungsmeldung
+
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      // Hier kannst du die Logik zum Senden des Formulars hinzufügen
+
+      // Zeige die Bestätigungsmeldung an
+      this.showConfirmation();
+    }
+  }
+
+  showConfirmation() {
+    this.confirmationVisible = true;
+
+    // Verstecke die Nachricht nach 3 Sekunden wieder
+    setTimeout(() => {
+      this.confirmationVisible = false;
+    }, 3000);
+  }
+ 
 }
