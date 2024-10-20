2aef66a581982c51ac1c6730cbd16f611561a394
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-10-20 16:59:23 +0200

    zweite abgabe

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,20 +5,20 @@ import { provideHttpClient, HttpClient, withFetch } from '@angular/common/http';
 import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-// Funktion zur Erstellung des TranslateHttpLoaders
+
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-// Erstelle die App-Konfiguration
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
       routes, 
-      withEnabledBlockingInitialNavigation(), // Router-Initialisierung mit Blockierung
-      withInMemoryScrolling({ // Scroll-Optionen
-        scrollPositionRestoration: 'enabled', // Scroll-Position wiederherstellen
-        anchorScrolling: 'enabled' // Anker-Link-Scrolling aktivieren
+      withEnabledBlockingInitialNavigation(), 
+      withInMemoryScrolling({ 
+        scrollPositionRestoration: 'enabled', 
+        anchorScrolling: 'enabled' 
       })
     ),
     provideHttpClient(withFetch()),
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-  { path: '', component: MainpageComponent },  // Hauptseite mit allen Abschnitten
-  { path: 'legal-notice', component: LegalNoticeComponent },  // Impressum
-  { path: 'privacy-policy', component: PrivacyPolicyComponent },  // Datenschutz
-  { path: '**', redirectTo: '', pathMatch: 'full' }  // Weiterleitung auf Hauptseite bei ungültiger Route
+  { path: '', component: MainpageComponent },  
+  { path: 'legal-notice', component: LegalNoticeComponent },  
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },  
+  { path: '**', redirectTo: '', pathMatch: 'full' } 
 ];
 
-// Extra Optionen, um Scroll-Verhalten zu steuern
+
 export const routerOptions: ExtraOptions = {
-  scrollPositionRestoration: 'enabled',  // Scrollt automatisch zu vorheriger Position
-  anchorScrolling: 'enabled',  // Aktiviert das Scrollen zu Ankern/Fragmenten
-  scrollOffset: [0, 64]  // Optional: Scrollen kann um einen Pixel-Offset verschoben werden, z.B. wegen einer fixierten Navigation
+  scrollPositionRestoration: 'enabled', 
+  anchorScrolling: 'enabled',  
+  scrollOffset: [0, 64]  
 };
