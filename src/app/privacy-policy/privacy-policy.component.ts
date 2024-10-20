d48fdd0a6ee56b336123091277f021cde7415b1c
Hasan Korkmaz <hasan_k2023@outlook.com>
2024-10-20 15:51:27 +0200

    button fix

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { HeaderComponent } from "../shared/components/header/header.component";
+import { TranslateModule } from '@ngx-translate/core';
+import { inject } from '@angular/core';
+import { TranslationService } from '../translation.service' ;
+
 
 @Component({
   selector: 'app-privacy-policy',
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss'],
   standalone: true,
-  imports: [RouterModule, HeaderComponent] 
+  imports: [RouterModule, HeaderComponent, TranslateModule] 
  
 })
 export class PrivacyPolicyComponent {
@@ -18,6 +22,8 @@ export class PrivacyPolicyComponent {
    
     window.scrollTo(0, 0);
   }
+
+  translate = inject(TranslationService);
 
   goBack() {
     this.router.navigate(['/']); 
